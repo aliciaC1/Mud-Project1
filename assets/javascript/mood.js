46740b9e8e4d2e03ecb3070994173fca914b7793
Alicia <chan.aliciaw@gmail.com>
2018-08-02 20:34:56 -0400

    added base html / css / js

---
+++ assets/javascript/mood.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  M.AutoInit();
+  // $('.sidenav').sidenav();
+  //   $('select').formSelect();
+
+    // $( function() {
+    //   $( "#draggable" ).draggable();
+    // } );
+
+  
+    $('.resizeDiv')
+	.draggable()
+  .resizable();
+   $('iframe').draggable().resizeable();
+  
+});
+      
